12b4b2c7f86af6807da5d5ebee5e98aab8f633c5
Chanakarn Limprasertsiri <minklim47@gmail.com>
2021-10-23 18:45:01 +0700

    layout

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 var textArea = document.getElementById("text");
+var imageArea = document.getElementById("image")
 
-var movieList = ["Titanic","Lego","Love, Rosie","d","fsf"];
+var movieList = ["Titanic","Lego","Love, Rosie","d","fsffffffffffffffffffffffffffffffffffffffffffffffffffffffffsafssafsfa"];
+var imageList = ["images/free_guy.jpg"]
 
 function getMovie(){
     let randomPick = movieList[Math.floor(Math.random() * movieList.length)];
-    textArea.innerHTML = randomPick;
+    textArea.innerHTML = movieList[0]; //randomPick;
+    imageArea.innerHTML = imageList[0];
 
     let index = movieList.indexOf(randomPick);
     movieList.splice(index,1);
